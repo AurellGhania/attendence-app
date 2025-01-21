1a0c1cc968e1571aa52ad1d785f754c661c80d8f
Aurell Ghania <aurellghania@Aurells-MacBook-Pro.local>
2025-01-21 09:10:38 +0700

    initial config for Google Ml KIt

---
+++ lib/utils/google_ml_kit.dart
@@ -0,0 +1,8 @@
+import 'package:attendance_app/utils/vision.dart';
+
+class GoogleMlKit {
+  //ini kita buat dia menjadi private
+  GoogleMlKit._();
+
+  static final Vision vision = Vision.instance;
+}
