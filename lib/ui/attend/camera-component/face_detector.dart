f6285fb07b67b1024d1c69a05712124beb9f4c9b
Aurell Ghania <aurellghania@Aurells-MacBook-Pro.local>
2025-02-03 10:57:18 +0700

    config face_detection

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,3 +1,11 @@
+import 'package:attendance_app/utils/google_ml_kit.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
 class FaceDetectorComponent {
-  
+  final FaceDetector faceDetector = GoogleMlKit.vision.faceDetector(FaceDetectorOptions(
+    enableContours: true,
+    enableClassification: true,
+    enableTracking: true,
+    enableLandmarks: true,
+  ));
 }
